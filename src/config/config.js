cf93c1f911852fcfe235143be5bfccf45c622605
zen-techn-143 <office.zentexus@gmail.com>
2025-12-09 12:58:49 +0530

    `Switched API domain between localhost and production URL`

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-// var API_DOMAIN = "https://diwalichit.zentexus.in/zenchitbilling/api";
-var API_DOMAIN = "http://localhost/hemnath_chit_fund_backend/api";
+var API_DOMAIN = "https://diwalichit.zentexus.in/zenchitbilling/api";
+// var API_DOMAIN = "http://localhost/hemnath_chit_fund_backend/api";
 
 export default API_DOMAIN;
